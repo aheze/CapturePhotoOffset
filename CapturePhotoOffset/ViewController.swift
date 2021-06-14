cdde28fe7e184152e7eb78b60b00d1d25e95dfb0
Zheng <aheze@getfind.app>
2021-06-13 17:24:00 -0700

    Initial Commit

---
+++ CapturePhotoOffset/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CapturePhotoOffset
+//
+//  Created by Zheng on 6/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
